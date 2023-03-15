09f70d29e59b4ca2e34ec5ff7f7613869f4e40fa
calixlock <calixlock@naver.com>
2023-03-15 23:50:46 +0900

    version5_listMessage_upgrade

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -27,12 +27,23 @@ public class App {
 
                 textBook book = new textBook(id, text, authorName);
                 textBooks.add(book);
-                HashMap<String, String> textBook = new HashMap<String, String>();
-                textBook.put(text.toString(),authorName.toString());
+//                HashMap<String, String> textBook = new HashMap<String, String>();
+//                textBook.put(text.toString(),authorName.toString());
                 lastTextNumber = id;
                 System.out.printf("%d번 명언이 등록되었습니다.\n",lastTextNumber);
             }else if (cmd.equals("목록")) {
-                System.out.printf("생성된 명언 수 : %d\n", textBooks.size());
+//                System.out.printf("생성된 명언 수 : %d\n", textBooks.size());
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
+
+                for (int i = textBooks.size() -1; i >=0; i--){
+                    textBook read_textbook = textBooks.get(i);
+
+                    System.out.printf("%d / %s / %s\n",read_textbook.getId(),read_textbook.getAuthorName(),read_textbook.getText());
+                }
+            }else if (cmd.equals("삭제")) {
+                System.out.printf("몇 번 명언을 삭제하시겠습니까? : \n");
+                int id = sc.nextInt();
             }
 
         }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 ==");
         Scanner sc = new Scanner(System.in);
-        App v5 = new App(sc);
-        v5.run();
+        App v5_1 = new App(sc);
+        v5_1.run();
 
         sc.close();
     }
